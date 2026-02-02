8468874fc326f09e9fa73382e4564c264010b2e7
HBrauer <hbrauer@gmx.de>
2026-02-02 13:51:34 +0100

    Add updated style

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,4 @@
+import './style.css';
+
 export { SpectrumWaterfall } from '@/components/sdr/SpectrumWaterfall';
 export type { SpectrumWaterfallProps, SpectrumData } from '@/components/sdr/SpectrumWaterfall';
